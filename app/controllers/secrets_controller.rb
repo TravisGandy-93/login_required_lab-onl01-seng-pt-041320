9adf09b9510e4b2040e22a92fc1c72b79cb3c62c
Travis Gandy <travis_gandy@yahoo.com>
2020-10-22 05:51:53 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,15 +1,13 @@
 class SecretsController < ApplicationController
   before_action :require_login
   def show
-    if !session[:name]
-      redirect_to '/login'
-    end
+  
   end
   
   private
 
 
   def require_login
-    return redirect_to '/login' unless session.include?(:name)
+    return redirect_to (controller: 'sessions', action: 'new') unless session.include?(:name)
   end
 end
